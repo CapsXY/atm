97edd43ae00f3b00c16dd8485966166ee328af1e
Caio Souza <caio.alberto@live.com>
2022-12-25 13:10:51 -0300

    refactoring code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	buttonLeftDisplay: {
-		background: 'bg-[#b4b4b4]',
-		width: 'w-[30px]',
-		height: 'h-[20px]',
-		borderRadius: 'rounded-[2px]',
-	}
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+
+		},
+  },
+  plugins: [
+
+	],
 }
